34cfe1b3359795951cc4b10a6d953e0a9a72f05f
psychemist <ikeagudike@gmail.com>
2024-10-12 11:05:31 +0100

    Add user contracts

---
+++ contracts/HealthNode.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+contract HealthNode {
+	struct Patient {
+
+	}
+
+	struct Physician {
+
+	}
+
+	struct Clinic {
+
+	}
+
+	struct Examination {
+
+	}
+
+	mapping(address user => mapping(address physician => bool)) hasAccess;
+
+	function registerUser()  external {
+
+	}
+
+	function 
+}
